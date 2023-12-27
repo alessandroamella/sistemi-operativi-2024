1ee55dd80c86209f406113210c3f77e19a20e7bc
Alessandro Amella <info@bitrey.it>
2023-12-27 13:52:24 +0100

    script per down

--- build_phase1.sh
+++ build_phase1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make clean && tar czvf phase1.tar.gz AUTHORS README.md Makefile phase1/ headers/ 
+sudo apt update && sudo apt install umps3 -y && make clean && tar czvf phase1.tar.gz AUTHORS README.md Makefile phase1/ headers/ 
